5962bde9ad7dd2088a427ee1f7206090e15073b9
Dipanjan Patra <patraanjan23@gmail.com>
2020-05-10 18:05:54 +0530

    finally added functionality

--- app.py
+++ app.py
@@ -1,23 +1,84 @@
 import os
-from flask import Flask
+import requests
+import operator
+import re
+import nltk
+from flask import Flask, request, render_template
 from flask_sqlalchemy import SQLAlchemy
-
+from sqlalchemy.dialects.postgresql import JSON
+from flask_migrate import Migrate
+from stop_words import stops
+from collections import Counter
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
-from models import Result
-
-@app.route('/')
-def hello():
-    return "Hello World!"
+migrate = Migrate(app, db)
 
 
-@app.route('/<name>')
-def hello_name(name):
-    return f"Hello {name}!"
+class Result(db.Model):
+    __tablename__ = 'results'
+
+    id = db.Column(db.Integer, primary_key=True)
+    url = db.Column(db.String())
+    result_all = db.Column(JSON)
+    result_no_stop_words = db.Column(JSON)
+
+    def __init__(self, url, result_all, result_no_stop_words):
+        self.url = url
+        self.result_all = result_all
+        self.result_no_stop_words = result_no_stop_words
+
+    def __repr__(self):
+        return f"<id {self.id}>"
 
 
-if __name__ == "__main__":
-    app.run()
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    errors = []
+    results = {}
+    if request.method == 'POST':
+        # get url that the user has entered
+        try:
+            url = request.form['url']
+            r = requests.get(url)
+        except:
+            errors.append(
+                "Unable to get URL. Please make sure it's valid and try again."
+            )
+            return render_template('index.html', errors=errors, results=results)
+        if r:
+            # text processing
+            raw = BeautifulSoup(r.text, 'html.parser').get_text()
+            nltk.data.path.append('./nltk_data/') # set the path
+            tokens = nltk.word_tokenize(raw)
+            text = nltk.Text(tokens)
+            # remove punctuation
+            nonPunct = re.compile('.*[A-Za-z].*')
+            raw_words = [w for w in text if nonPunct.match(w)]
+            raw_word_count = Counter(raw_words)
+            # stop words 
+            no_stop_words = [w for w in raw_words if w.lower() not in stops]
+            no_stop_words_count = Counter(no_stop_words)
+            #save the results
+            results = sorted(
+                no_stop_words_count.items(),
+                key=operator.itemgetter(1),
+                reverse=True
+            )[:10]
+            try:
+                result = Result(
+                    url=url,
+                    result_all=raw_word_count,
+                    result_no_stop_words=no_stop_words_count
+                )
+                db.session.add(result)
+                db.session.commit()
+            except:
+                errors.append("Unable to add item to database.")
+        return render_template('index.html', errors=errors, results=results)
+
+    return render_template('index.html', errors=errors, results=results)
